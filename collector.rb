f7cdc81bce174c176435c466ad737be3f2a9efed
marmeladze <wrested@hotmail.de>
2017-10-17 10:59:12 +0400

    Update collector.rb

--- collector.rb
+++ collector.rb
@@ -2,23 +2,21 @@ require 'twitter'
 
 while true
 begin
-    config = {
-        :consumer_key => "CONSUMER_KEY",
-        :consumer_secret => "CONSUMER_SECRET",
-        :access_token => "ACCESS_TOKEN",
-        :access_token_secret => "ACCESS_TOKEN_SECRET",
-    }
-
-    sClient = Twitter::Streaming::Client.new(config)
-
-    topics = ['#plazadili', 'plazadili']
-    sClient.filter(:track => topics.join(',')) do |tweet|
-        if tweet.is_a?(Twitter::Tweet)
-          puts "#{tweet.user.screen_name}: #{tweet.text}"
-        end
+  config = {
+    :consumer_key => "CONSUMER_KEY",
+    :consumer_secret => "CONSUMER_SECRET",
+    :access_token => "ACCESS_TOKEN",
+    :access_token_secret => "ACCESS_TOKEN_SECRET",
+  }
+  sClient = Twitter::Streaming::Client.new(config)
+  topics = ['#plazadili', 'plazadili']
+  sClient.filter(:track => topics.join(',')) do |tweet|
+    if tweet.is_a?(Twitter::Tweet)
+      puts "#{tweet.user.screen_name}: #{tweet.text}"
     end
+  end
 rescue Exception => e
-    puts 'error, waiting 2s: ' + e.class.to_s
-    sleep 2
+  puts 'error, waiting 2s: ' + e.class.to_s
+  sleep 2
 end
 end
